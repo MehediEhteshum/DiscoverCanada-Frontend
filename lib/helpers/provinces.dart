0dc13ed3567f879a1c69c0886b6b80f55777affa
Mehedi Ehteshum <me69oshan@gmail.com>
2021-04-04 23:14:28 -0600

    offline provinces list;

--- lib/helpers/provinces.dart
+++ lib/helpers/provinces.dart
@@ -1,40 +1,66 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:hive/hive.dart';
 
 import './base.dart';
 
+Future<Box> Function() _openProvincesBox = () async {
+  return await Hive.openBox("provinces");
+};
+
 Future<dynamic> fetchProvinces(bool isOnline) async {
   Future<dynamic> error;
-  List<String> _provinces = [];
 
-  if (isOnline) {
-    // if online, fetch from internet
-    try {
-      String provincesUrl =
-          "${FlutterConfig.get('BASE_URL')}:${FlutterConfig.get('PORT')}/discover-canada/api/provinces";
-      final Response response =
-          await Dio().get(provincesUrl).timeout(Duration(seconds: timeOut));
-      if (response.statusCode == successCode) {
-        final extractedData = response.data;
-        if (extractedData["data"] != null) {
-          extractedData["data"].forEach((_provinceObj) => {
-                _provinces.add(
-                  _provinceObj["name"],
-                ),
-              });
-          provinces = [..._provinces]; // assigning to global variable
-          throw ("NoError");
+  await _openProvincesBox().then((Box provincesBox) async {
+    if (isOnline) {
+      // if online, fetch from internet
+      try {
+        String provincesUrl =
+            "${FlutterConfig.get('BASE_URL')}:${FlutterConfig.get('PORT')}/discover-canada/api/provinces";
+        final Response response =
+            await Dio().get(provincesUrl).timeout(Duration(seconds: timeOut));
+        if (response.statusCode == successCode) {
+          final dynamic extractedData = response.data;
+          if (extractedData["data"].length > 0) {
+            await _storeProvincesData(provincesBox, extractedData["data"]);
+            final List<String> _provinces =
+                _createProvincesList(extractedData["data"]);
+            provinces = [..._provinces]; // assigning to global variable
+            throw ("NoError");
+          } else {
+            throw ("EmptyData");
+          }
+        } else {
+          throw ("Error loading data: ${response.statusCode}");
         }
-      } else {
-        throw ("Error loading data: ${response.statusCode}");
+      } catch (e) {
+        error = Future.error(e.toString());
       }
-    } catch (e) {
-      error = Future.error(e.toString());
+    } else {
+      // if offline, fetch from device
+      dynamic provincesData = provincesBox.toMap().values.toList();
+      final List<String> _provinces = _createProvincesList(provincesData);
+      provinces = [..._provinces]; // assigning to global variable
+      throw ("NoError");
     }
-  } else {
-    // if offline, fetch from device
-    error = Future.error("Offline");
-  }
+  }).catchError((e) {
+    error = Future.error(e.toString());
+  });
 
   return error;
+}
+
+Future<void> _storeProvincesData(Box provincesBox, dynamic data) async {
+  await provincesBox.clear(); // clear previous data
+  data.forEach((provinceObj) => {
+        provincesBox.add(provinceObj),
+      });
+}
+
+List<String> _createProvincesList(dynamic data) {
+  final List<String> _provinces = [];
+  data.forEach((provinceObj) => {
+        _provinces.add(provinceObj["name"]),
+      });
+  return _provinces;
 }
