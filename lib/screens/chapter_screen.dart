5fd5af87ce6f60de156b47e9a649d6cec61a1ffe
Mehedi Ehteshum <me69oshan@gmail.com>
2021-04-28 19:18:33 -0600

    sample.pdf viewing done; testing controls;

--- lib/screens/chapter_screen.dart
+++ lib/screens/chapter_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
 
 import '../helpers/base.dart';
 import '../widgets/selection_info.dart';
 import '../widgets/retry.dart';
 import '../widgets/no_internet_message.dart';
 import '../widgets/coming_soon_message.dart';
+import '../models and providers/internet_connectivity_provider.dart';
 
-class ChapterScreen extends StatelessWidget {
+class ChapterScreen extends StatefulWidget {
   static const routeName = "/chapter";
+
+  @override
+  _ChapterScreenState createState() => _ChapterScreenState();
+}
+
+class _ChapterScreenState extends State<ChapterScreen> {
   static bool _isLoading = false;
   static String _error = "NoError";
+  bool _containsPdf = (selectedChapter.pdfUrl != null);
+  PdfController _pdfController;
+
+  @override
+  void initState() {
+    if (_containsPdf) {
+      print("pdf chapter");
+      _pdfController = PdfController(
+        document: PdfDocument.openAsset('assets/pdfs/sample.pdf'),
+      );
+    }
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    setState(() {
+      isOnline = Provider.of<InternetConnectivity>(context).isOnline;
+      _refreshWidget(); // as soon as online/offline, it refreshes widget
+    });
+    super.didChangeDependencies();
+  }
 
   Future<void> _refreshWidget() async {
     // setState(() {
@@ -41,7 +72,7 @@ class ChapterScreen extends StatelessWidget {
                     "${selectedChapter.title}",
                     softWrap: true,
                   ),
-                  floating: true,
+                  pinned: true,
                   expandedHeight: screenWidth *
                       0.4, // proportional to screen width = AppBar kToolbarHeight + Topic title w/ extra 1 line + Province name + Bottom padding statusbarHeight
                   flexibleSpace: const FlexibleSpaceBar(
@@ -58,7 +89,19 @@ class ChapterScreen extends StatelessWidget {
                 ),
                 (_error == "NoError")
                     ? SliverFillRemaining(
-                        child: ComingSoonMessage(),
+                        child: _containsPdf
+                            ? PdfView(
+                                controller: _pdfController,
+                                documentLoader: Center(
+                                  child: const CircularProgressIndicator(),
+                                ),
+                                pageLoader: Center(
+                                  child: const CircularProgressIndicator(),
+                                ),
+                                scrollDirection: Axis.vertical,
+                                physics: BouncingScrollPhysics(),
+                              )
+                            : ComingSoonMessage(),
                       )
                     : SliverFillRemaining(
                         child: Retry(refreshWidget: _refreshWidget),
@@ -66,5 +109,13 @@ class ChapterScreen extends StatelessWidget {
               ],
             ),
     );
+  }
+
+  @override
+  void dispose() {
+    if (_containsPdf) {
+      _pdfController.dispose();
+    }
+    super.dispose();
   }
 }
